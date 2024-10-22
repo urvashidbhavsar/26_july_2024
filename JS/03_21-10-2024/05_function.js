419cb93d5df3e1bc3cf2f35ef179a11e38ba6b7b
urvashi <urvashi.d.bhavsar@gmail.com>
2024-10-22 08:15:45 +0530

    22nd changes

---
+++ JS/03_21-10-2024/05_function.js
@@ -0,0 +1,11 @@
+function msg() {
+    console.log("Hello world");
+}
+msg()
+msg()
+
+function newMsg(no, msg) {
+    console.log(no + " => " + msg);
+}
+newMsg(1, "Hello world!!!!!")
+newMsg(2, "Learn Js")
